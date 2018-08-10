8e4d4c79a342fc04fcf98a8d9e39fddae912b787
Adam P <adamparalovos@hotmail.com>
2018-08-10 01:22:30 -0400

    added ability to load 10 more gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,17 @@ $(document).ready(function () {
 
     // Initial array of Marvel characters
     var marvelArray = ["Scarlet Witch", "Vision", "Quicksilver", "Iron Man", "Spider-Man", "The Wasp", "Ant-Man", "Black Widow", "Hawkeye", "Doctor Strange", "Okoye", "Black Panther", "Shuri", "Star-Lord", "Rocket Raccoon", "Drax", "Mantis", "Nebula", "Gamora", "Captain America", "Groot", "Thor", "Loki", "Venom", "Daredevil", "Storm", "Wolverine", "Mystique", "Magneto", "Thanos", "Deadpool", "The Hulk", "Captain Marvel"];
-
+    var limit = 10;
+    var newLimit = 10;
     // Function for dumping the JSON content for each button into the div
     function displayCharacters() {
-
         $("#marvelCharacters").empty();
-
+        $("#moreGifs").empty()
+        newLimit = 10;
         var characterName = $(this).attr("data-name");
         var nameNoSpace = characterName.replace(/ /g, "%20");
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nameNoSpace + "%20Marvel&api_key=xy4lY11exI2GePmdAFdsV1EpMSuNXop4&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nameNoSpace + "%20Marvel&api_key=xy4lY11exI2GePmdAFdsV1EpMSuNXop4&limit=" + limit;
         console.log(queryURL);
         // AJAX call
         $.ajax({
@@ -19,14 +20,11 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-
-
             for (var i = 0; i < response.data.length; i++) {
                 // Creating a div to hold the charater
                 var characterDiv = $("<div>");
                 characterDiv.addClass("card");
                 characterDiv.attr("style", "float:left; margin: 20px 20px ;");
-
 
                 // Retrieving the URL for the still image
                 var imgURL = response.data[i].images.fixed_height_still.url;
@@ -72,6 +70,8 @@ $(document).ready(function () {
 
                 characterDiv.append(cardBody);
 
+
+
                 // Putting the entire character above the previous characters
                 $("#marvelCharacters").append(characterDiv);
             }
@@ -79,6 +79,19 @@ $(document).ready(function () {
         }).catch(function (error) {
             console.log(error);
         });
+
+        if ($("#moreGifs").html() === "") {
+            // adding the load more gifs button
+            var moreButton = $("<button>");
+            // Adding a class of charcater
+            moreButton.addClass("more-gifs");
+            // Adding a data-attribute with a value of the charcater at index i
+            moreButton.attr("data-name", characterName);
+            // Providing the button's text with a value of the character at index i
+            moreButton.text("Load 10 more gifs");
+            // Adding the button to the HTML
+            $("#moreGifs").append(moreButton);
+        }
 
     }
 
@@ -136,14 +149,14 @@ $(document).ready(function () {
     // Function to save data to local storage
     function saveMarvelArray() {
         var str = JSON.stringify(marvelArray);
-        localStorage.setItem("marvelArray",str)
+        localStorage.setItem("marvelArray", str)
     }
 
     // Function to get data from local storage
     function getMarvelArray() {
         var str = localStorage.getItem("marvelArray");
         marvelArray = JSON.parse(str);
-        if (!marvelArray){
+        if (!marvelArray) {
             marvelArray = ["Scarlet Witch", "Vision", "Quicksilver", "Iron Man", "Spider-Man", "The Wasp", "Ant-Man", "Black Widow", "Hawkeye", "Doctor Strange", "Okoye", "Black Panther", "Shuri", "Star-Lord", "Rocket Raccoon", "Drax", "Mantis", "Nebula", "Gamora", "Captain America", "Groot", "Thor", "Loki", "Venom", "Daredevil", "Storm", "Wolverine", "Mystique", "Magneto", "Thanos", "Deadpool", "The Hulk", "Captain Marvel"];
 
         }
@@ -151,7 +164,88 @@ $(document).ready(function () {
 
     // Function to display gifs on page when character button is clicked
     $(document).on("click", ".character", displayCharacters);
+    $(document).on("click", ".more-gifs", displayAgain);
+
+
+    function displayAgain() {
+
+        $("#marvelCharacters").empty();
+
+        var characterName = $(this).attr("data-name");
+        var nameNoSpace = characterName.replace(/ /g, "%20");
+        newLimit = newLimit + 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nameNoSpace + "%20Marvel&api_key=xy4lY11exI2GePmdAFdsV1EpMSuNXop4&limit=" + newLimit;
+        console.log(queryURL);
+        // AJAX call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            for (var i = 0; i < response.data.length; i++) {
+                // Creating a div to hold the charater
+                var characterDiv = $("<div>");
+                characterDiv.addClass("card");
+                characterDiv.attr("style", "float:left; margin: 20px 20px ;");
+
+                // Retrieving the URL for the still image
+                var imgURL = response.data[i].images.fixed_height_still.url;
+
+                // Creating an element to hold the image
+                var image = $("<img>").attr("src", imgURL);
+                image.attr("style", "height:200px;");
+                image.attr("data-still", response.data[i].images.fixed_height_still.url);
+                image.attr("data-animate", response.data[i].images.fixed_height.url);
+                image.attr("data-state", "still");
+                image.addClass("card-img-top gif");
+
+                // Appending the image
+                characterDiv.append(image);
+
+                var cardBody = $("<div>");
+                cardBody.addClass("card-body");
+
+
+                // Storing the rating data
+                var rating = response.data[i].rating;
+                var title = response.data[i].title;
+                var download = response.data[i].images.fixed_height.url;
+
+                // Creating an element to have the rating displayed
+                var cardText = $("<span>").text(" Rating: " + rating);
+                var cardTitle = $("<h5>").text(title);
+                var downloadText = $("<a download>").text(" Download Gif");
+                downloadText.attr("href", download);
+                cardText.addClass("card-text");
+                cardTitle.addClass("card-title");
+                downloadText.addClass("card-download");
+
+                var downloadIcon = $("<i>");
+                downloadIcon.addClass("fas fa-download");
+
+                var ratingIcon = $("<i>");
+                ratingIcon.addClass("fas fa-star");
+
+                var lineBreak = $("<br />");
+
+                cardBody.append(cardTitle).append(ratingIcon).append(cardText).append(lineBreak).append(downloadIcon).append(downloadText);
+
+                characterDiv.append(cardBody);
+
+
+
+                // Putting the entire character above the previous characters
+                $("#marvelCharacters").append(characterDiv);
+            }
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+
+
+    }
 
     renderButtons();
-    
+
 });
